72690fa0c5bf720d3a29a102519dde7f5ecd4eef
najmuleimon <najmul.eimon@gmail.com>
2022-03-12 23:13:00 +0600

    file update

--- js/script.js
+++ js/script.js
@@ -18,6 +18,10 @@ const toggleSpinner = displayStyle => {
 const toggleSearchResult = displayStyle => {
     document.getElementById('all-drinks').style.display = displayStyle;
 }
+// toggle rest drinks
+const toggleRestDrinks = displayStyle => {
+    document.getElementById('rest-drinks').style.display = displayStyle;
+}
 // toggle load more button
 const toggleLoadMore = displayStyle => {
     document.getElementById('load-more').style.display = displayStyle;
@@ -38,6 +42,8 @@ const searchDrink = () => {
     toggleSpinner('block')
     // drinks view off
     toggleSearchResult('none')
+    // toggle rest drinks
+    toggleRestDrinks('none')
     // toggle load more
     toggleLoadMore('none')
 
@@ -48,7 +54,6 @@ const searchDrink = () => {
     .then(res => res.json())
     .then(data => displayDrinks(data.drinks))
 }
-
 // display drinks on html
 const displayDrinks = drinks => {
     const allDrink = document.getElementById('all-drinks')
@@ -56,7 +61,6 @@ const displayDrinks = drinks => {
     allDrink.textContent = ''
 
     const firstTen = drinks.slice(0, 10)
-    const restAll = drinks.slice(10)
     errorMsg.innerText = `Showing ${firstTen.length} results from total ${drinks.length} search result found`
 
     firstTen.forEach(drink => {
@@ -68,7 +72,7 @@ const displayDrinks = drinks => {
         <h3>${drink.strCategory}</h3>
         <p>${drink.strInstructions}</p>
         <p>${drink.strIngredient1}, ${drink.strIngredient2}, ${drink.strIngredient3}, ${drink.strIngredient4}, ${drink.strIngredient5}, ${drink.strIngredient6}, ${drink.strIngredient7}, ${drink.strIngredient8}, ${drink.strIngredient9}</p>
-        <button onclick="showDetail(${drink.idDrink})">see details</button>
+        <button data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="showDetail(${drink.idDrink})" class="btn btn-primary">see details</button>
         `
         allDrink.appendChild(div)
     })
@@ -76,22 +80,33 @@ const displayDrinks = drinks => {
     // loader off
     toggleSpinner('none')
     // drinks view on
-    toggleSearchResult('block')
-    // toggle load more
-    toggleLoadMore('block')
-    loadMore(restAll)
+    toggleSearchResult('flex')
+    if(drinks.length > 10){
+        // toggle load more
+        toggleLoadMore('block')
+    }
+    
+    
+    loadMore(drinks)
 }
-const showDetail = (id) => {
+
+// show drinks details by id on button click
+const showDetail = async id => {
     const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
-    fetch(url)
-    .then(res => res.json())
-    .then(data => console.log(data.drinks[0]))
+    const res = await fetch(url)
+    const data = await res.json()
+    displayDetail(data.drinks[0])
 }
 
 const loadMore = restAll => {
     const loadMoreBtn = document.getElementById('load-more')
+    
+    
+    // const allDrink = document.getElementById('all-drinks')
     loadMoreBtn.addEventListener('click', function(){
-        restAll.forEach(drink => {
+        const restDrinks = document.getElementById('rest-drinks')
+        restDrinks.textContent = ''
+        restAll.slice(10).forEach(drink => {
             let div = document.createElement('div')
             div.classList.add('col-md-4')
             div.innerHTML = `
@@ -100,9 +115,23 @@ const loadMore = restAll => {
              <h3>${drink.strCategory}</h3>
              <p>${drink.strInstructions}</p>
              <p>${drink.strIngredient1}, ${drink.strIngredient2}, ${drink.strIngredient3}, ${drink.strIngredient4}, ${drink.strIngredient5}, ${drink.strIngredient6}, ${drink.strIngredient7}, ${drink.strIngredient8}, ${drink.strIngredient9}</p>
-             <button onclick="showDetail(${drink.idDrink})">see details</button>
+             <button data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="showDetail(${drink.idDrink})" class="btn btn-primary">see details</button>
              `
-             allDrink.appendChild(div)
+             restDrinks.appendChild(div)
          })
+         toggleLoadMore('none')
+         // toggle rest drinks
+        toggleRestDrinks('flex')
     })
+}
+
+// display drink details on modal
+const displayDetail = (drinks) => {
+    const modalBody = document.getElementById('modal-body')
+    modalBody.innerHTML = `
+    <img src="${drinks.strDrinkThumb}" alt="">
+    <h2>${drinks.strDrink}</h2>
+    <h3>${drinks.strCategory}</h3>
+    <p>${drinks.strInstructions}</p>
+    `
 }
